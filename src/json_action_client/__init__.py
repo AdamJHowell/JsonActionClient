9468ee5c6303dce454f2745802d24a960fd7f90e
Adam Howell <AdamJHowell@users.noreply.github.com>
2026-02-17 16:56:06 -0700

    Update __init__.py

--- src/json_action_client/__init__.py
+++ src/json_action_client/__init__.py
@@ -2,10 +2,10 @@
 # JsonActionClient/src/json_action_client/__init__.py
 
 # Expose the main classes so users can import them directly
-from .client import JsonActionClient
-from .client import JsonActionError
 from .client import JsonActionApiError
+from .client import JsonActionClient
 from .client import JsonActionConnectionError
+from .client import JsonActionError
 
 __all__ = [
     "JsonActionClient",
